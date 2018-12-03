e08c8bf24ddf790c8020a8955bfbe4c5a94befcc
bjornkai <bjornkairoland@gmail.com>
2018-12-03 18:41:26 -0500

    not done

--- js/fruit.js
+++ js/fruit.js
@@ -1,20 +1,12 @@
 const myCanvas = document.querySelector(".fruit-master");
 const ctx = myCanvas.getContext("2d");
 
-// BACKGROUND 
-document.getElementById("fruit-master").style.background = "";
-
-myCanvas.width = 903;
-myCanvas.height = 657;
+const backgroundImage = new Image();
+backgroundImage.src = 
 
 // GLOBAL VARIABLES 
 let score = 0;
 
-
 // GAME CONSTRUCTOR FUNCTION
-const Game = function(){ 
-}
-
-
 
 // LOAD IMAGE FIRST or NOTHING WILL DRAW.
