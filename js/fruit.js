a66c1a2b7da034fa9c187743f314ab4a9f9e34b2
bjornkai <bjornkairoland@gmail.com>
2018-12-04 17:48:24 -0500

    add fruits and bomb

--- js/fruit.js
+++ js/fruit.js
@@ -2,10 +2,27 @@ const myCanvas = document.querySelector(".fruit-master");
 const ctx = myCanvas.getContext("2d");
 
 const backgroundImage = new Image();
-backgroundImage.src = 
+backgroundImage.src = `/images/curtain.png`;
 
 // GLOBAL VARIABLES 
 let score = 0;
+
+// FRUIT and BOMB ARRAY/OBJECT
+const fruitsArray = [
+  {name: `apple`, image: `/images/apple.png`},
+  {name: `banana`, image: `/images/banana.png`},
+  {name: `blueberries`, image: `/images/blueberries.png`},
+  {name: `grapes`, image: `/images/grapes.png`},
+  {name: `orange`, image: `/images/orange`},
+  {name: `peaches`, image: `/images/peaches.png`},
+  {name: `pear`, image: `/images/pear.png`},
+  {name: `watermelon`, image: `/images/watermelon.png`}
+];
+
+const bomb = [
+  {name: `bomb`, image: `/images/bomb.png`}
+];
+
 
 // GAME CONSTRUCTOR FUNCTION
 
